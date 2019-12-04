83824eeb30cf01c5ff0a615e1207e9d10183533d
Guillermo Jimenez Perez <memo5775@hotmail.com>
2019-12-03 18:14:09 -0600

    terminado

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,9 @@
 console.log("Hola mundo");
+var PesoMexicano = 346
 
-PesoMexicano = 346
+var Dolares = 19.47
+var Euro = 21.68
+var CUC = 0.01957
+var PesoCubano = 19.59
 
-
-Dolares = 19.47
-console.log("PesoMexicano * Dolares");
-
-Euro = 21.68
-
-
-CUC = 0.01957
-PesoMexicano * Dolares / CUC
-
-
-
+//lo hice a traves de la consola
