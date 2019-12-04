b49affa23fdb271239e895dbcd0255319a4a5f70
Guillermo Jimenez Perez <memo5775@hotmail.com>
2019-12-03 21:06:37 -0600

    actualizacion clase

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,11 @@
 var word = "perros"
-word[0] + word[1] + word[2] + word[3] + word[4] 
+word[0] + word[1] + word[2] + word[3] + word[4]
+
+var number = 10
+if (number >= 0 && number <= 100 && number % 10 === 0) { console.log("entre o y 100 que sea multiplo de 10") }
+else if (number % 10 === 0) 
+else if (number >= 0 &&& number % 10 !==)
+    console.warn(es positivo, no es multiplo del 10)
+else {
+    console.error("no esta en el rango y no cumple")
+}
