577f92a0e521e9a88fb354aa9884c0d2c2e1daf2
Guillermo Jimenez Perez <memo5775@hotmail.com>
2019-12-04 19:12:43 -0600

    ultima actividad de clase 2

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,19 @@
-var word = "perros"
-word[0] + word[1] + word[2] + word[3] + word[4]
+var name = "Doctrina".length
 
-var number = 10
-if (number >= 0 && number <= 100 && number % 10 === 0) { console.log("entre o y 100 que sea multiplo de 10") }
-else if (number % 10 === 0) 
-else if (number >= 0 &&& number % 10 !==)
-    console.warn(es positivo, no es multiplo del 10)
-else {
-    console.error("no esta en el rango y no cumple")
+if (name % 2 === 0) {
+    if (name >= 0) {
+        if (name <= 10) {
+            console.log("Numero esta entre cero y diez")
+        } else if (name <= 15) {
+            console.log("El numero esta entre once y quince")
+        } else {
+            console.log("El numero es mayor que quince")
+        }
+    }
+} else {
+    console.log("No es par")
 }
+Contraer
+
+
+
