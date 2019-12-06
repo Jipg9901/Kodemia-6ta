9910787eda53710d63fc6d73943b4d96e9f01872
Guillermo Jimenez Perez <memo5775@hotmail.com>
2019-12-05 22:08:00 -0600

    actualizacionDeEjercicios

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo");
+/*for (var numeros = 0; numeros <= 100; numeros++) {
+    if (numeros % 2 === 0) { //si es vedad lo que esta entre los perentecis, se ejecuta lo que esta adentro de las llaves
+        console.log(`numero: ${numeros} es numero par`)
+    }
+    else { // si lo que estra adentro de if () anterior no se cumple se ejecuta en else dentro de {}  
+        console.log(`${numeros} no es par`)
+    }
+}
+*/
+var datos = [p, e, r, r, o]
+var index = datos[1]
+
+for (var index = 0; index < datos.length; datos++) {
+    console.log(`${datos[index][0]} ${datos[index][4]}`)
+}
