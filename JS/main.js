a262e626795489b4036904796acfdc0fe30c42ba
Guillermo Jimenez Perez <memo5775@hotmail.com>
2019-12-16 20:00:51 -0600

    fonal

---
+++ JS/main.js
@@ -0,0 +1,78 @@
+//var escuela = [
+//  {
+//  {
+//    `nombre`: carlos,
+//    `edad`: 20,
+//    `curp`: `jipg990118hdfmrl08`,
+//    `matematicas`: 5,
+//    `español`: 8,
+//   `ciencias`: 10,
+//   `promedioFinal`: 7.2
+// },
+// {
+//   `nombre`: `angelica`,
+//   `edad`: `20`,
+//    `curp`: `jipg990118hdfmrl08`,
+//    `matematicas`: 7,
+//    `español`: 6,
+//    `ciencias`: 10,
+//     `promedioFinal`: 8
+//  }
+//]
+//const promedioFinal = grupo => {
+//  return grupo.reduce((acc, cValue) => {
+//    return acc + cValue.asignacion.reduce((acc2, cValue2) => {
+//      return acc2 + cValue2.grade
+//    }, 0) / cValue.asignacion.length
+//  }, 0) / group.length
+//
+//}
+
+//const promedioFinal = grupo => {
+//  return grupo.reduce((acc, valor) => {
+//    return acc + valor.final
+//  }, 0) / grupo.length
+//}
+
+
+//clase 8
+
+//[
+//  [`Memo`, `Jimenez`],
+//  [`Pancho`,  `Lopez`],
+//  [`Pit`, `Zajot`]
+//]
+//
+//[ 
+//{
+//  firstName: `Nombre`,
+//  lastName: `Apellido`,
+//  edad: `78`
+//}
+//]
+//
+//function Table(material, color, precio){
+//  this.material = material
+//  this.color =color
+//  this.precio = precio
+//}
+
+
+// [
+//     [`Memo`, `Jimenez`,25],
+//     [`Pancho`,  `Lopez`,88],
+//     [`Pit`, `Zajot`,99]
+// ]
+
+// var datos =[]
+// function datos(nombre, apellido, edad){
+//   this.nombre = ``
+//   this.apellido = ``
+//   this.edad = ``
+// }
+
+// datos.map= new datos (`a`,`b`,`c`)
+// personalba
+
+var miltiplos = [n, n2]
+hacer que los multiplos sea cual sea podamos asegnarles 2 valores el numero mas el multiplicador
